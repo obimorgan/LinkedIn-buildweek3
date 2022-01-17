f2223729c0832cd370b663ee3c0ea4f7313b8cc0
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-17 15:56:21 +0100

    updated put endpont

--- src/Profiles/profiles.js
+++ src/Profiles/profiles.js
@@ -26,36 +26,6 @@ const cloudinaryStorage = new CloudinaryStorage({
 
 const parser = multer({ storage: cloudinaryStorage });
 
-//image upload endpoint
-profilesRouter.post(
-  "/:profileId/imageUpload",
-  parser.single("profileImage"),
-  async (req, res, next) => {
-    try {
-      3;
-      const profile = await ProfilesModel.findById(req.params.profileId);
-      if (profile) {
-        const profileToEdit = await ProfilesModel.findByIdAndUpdate(
-          req.params.profileId,
-          { $set: { imageUrl: req.file.path } },
-          { new: true }
-        );
-        await profileToEdit.save();
-        res.send(profileToEdit);
-      } else {
-        next(
-          createHttpError(
-            404,
-            `Review with id ${req.params.reviewId} does not exist or has been deleted.`
-          )
-        );
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
 profilesRouter.route("/").get(async (req, res, next) => {
   try {
     const profiles = await ProfilesModel.find();
@@ -72,7 +42,7 @@ profilesRouter.post(
   async (req, res, next) => {
     try {
       const newprofile = await new ProfilesModel(req.body);
-      newprofile.image = req.file.path || "";
+      newprofile.image = req.file ? req.file.path : req.body.image;
       await newprofile.save();
       res.status(201).send(newprofile);
     } catch (error) {
@@ -104,12 +74,18 @@ profilesRouter
       next(error);
     }
   })
-  .put(async (req, res, next) => {
+
+  .put(parser.single("profileImage"), async (req, res, next) => {
     try {
       const profilesId = req.params.profilesId;
+
+      const body = {
+        ...req.body,
+        image: req.file ? req.file.path : req.body.image,
+      };
       const editprofiles = await ProfilesModel.findByIdAndUpdate(
         profilesId,
-        req.body,
+        body,
         {
           new: true,
         }
@@ -149,4 +125,33 @@ profilesRouter
       next(error);
     }
   });
+//   //image upload endpoint
+// profilesRouter.post(
+//   "/:profileId/imageUpload",
+//   parser.single("profileImage"),
+//   async (req, res, next) => {
+//     try {
+//       3;
+//       const profile = await ProfilesModel.findById(req.params.profileId);
+//       if (profile) {
+//         const profileToEdit = await ProfilesModel.findByIdAndUpdate(
+//           req.params.profileId,
+//           { $set: { imageUrl: req.file.path } },
+//           { new: true }
+//         );
+//         await profileToEdit.save();
+//         res.send(profileToEdit);
+//       } else {
+//         next(
+//           createHttpError(
+//             404,
+//             `Review with id ${req.params.reviewId} does not exist or has been deleted.`
+//           )
+//         );
+//       }
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 export default profilesRouter;
