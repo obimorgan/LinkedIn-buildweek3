4c81d38b1ce4b10f4ec9650b750e91130eb1645f
martipurull <91315241+martipurull@users.noreply.github.com>
2022-01-17 13:36:02 +0100

    adds post schema and crud

--- src/Posts/posts.js
+++ src/Posts/posts.js
@@ -1,10 +1,128 @@
 /** @format */
-
-import mongoose from "mongoose";
 import express from "express";
+import PostModel from './schema.js'
+import createHttpError from 'http-errors'
+import q2m from 'query-to-mongo'
+import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
 
 const postsRouter = express.Router();
 
-postsRouter.route("/").get(async, (req, res, next));
+//cloudinary config
+const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET } = process.env
+
+cloudinary.config({
+    cloud_name: CLOUDINARY_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_SECRET
+})
+
+const cloudinaryStorage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'nft-products-mongo',
+    },
+});
+
+const parser = multer({ storage: cloudinaryStorage });
+
+//posts endpoints
+// postsRouter.post('/', async (req, res, next) => {
+//     try {
+//         const newPost = new PostModel(req.body)
+//         await newPost.save()
+//         res.status(201).send(newPost)
+//     } catch (error) {
+//         next(error)
+//     }
+// })
+
+//post + img
+postsRouter.post('/', parser.single('postImage'), async (req, res, next) => {
+    try {
+        const newPost = new PostModel(req.body)
+        const imgUrl = req.file.path
+        if (imgUrl) {
+            newPost.image = imgUrl
+            await newPost.save()
+            res.status(201).send(newPost)
+        } else {
+            await newPost.save()
+            res.status(201).send(newPost)
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+postsRouter.get('/', async (req, res, next) => {
+    try {
+        const mongoQuery = q2m(req.query)
+        const noOfPosts = await PostModel.countDocuments(mongoQuery.criteria)
+        const posts = await PostModel.find(mongoQuery.criteria)
+            .limit(mongoQuery.options.limit)
+            .skip(mongoQuery.options.skip)
+            .sort(mongoQuery.options.sort)
+        res.send({ link: mongoQuery.links('/posts', noOfPosts), pageTotal: Math.ceil(noOfPosts / mongoQuery.options.limit), noOfPosts, posts })
+    } catch (error) {
+        next(error)
+    }
+})
+
+postsRouter.get('/:postId', async (req, res, next) => {
+    try {
+        const foundPost = await PostModel.findById(req.params.postId)
+        if (foundPost) {
+            res.send(foundPost)
+        } else {
+            next(createHttpError(404, `This post no longer exists.`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+postsRouter.put('/:postId', async (req, res, next) => {
+    try {
+        const editedPost = await PostModel.findByIdAndUpdate(req.params.postId, req.body, { new: true })
+        if (editedPost) {
+            res.send(editedPost)
+        } else {
+            next(createHttpError(404, `This post no longer exists and cannot be edited.`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+postsRouter.delete('/:postId', async (req, res, next) => {
+    try {
+        const deletedPost = await PostModel.findByIdAndDelete(req.params.postId)
+        if (deletedPost) {
+            res.status(204).send()
+        } else {
+            next(createHttpError(404, `This post does not exist or has already been deleted.`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+//post image upload
+// postsRouter.post('/:postId', parser.single('postImage'), async (req, res, next) => {
+//     try {
+//         const post = await PostModel.findById(req.params.postId)
+//         if (post) {
+//             const postToAddImgTo = await PostModel.findByIdAndUpdate(req.params.postId, { $set: { image: req.file.path } }, { new: true })
+//             await postToAddImgTo.save()
+//             res.send(postToAddImgTo)
+//         } else {
+//             next(createHttpError(404, `This post does not exist.`))
+//         }
+//     } catch (error) {
+//         next(error)
+//     }
+// })
 
 export default postsRouter;
--- src/Posts/schema.js
+++ src/Posts/schema.js
@@ -1,1 +1,16 @@
 /** @format */
+import mongoose from 'mongoose'
+
+const { Schema, model } = mongoose
+
+const PostSchema = new Schema(
+    {
+        text: { type: String, required: true },
+        username: { type: String },
+        image: { type: String },
+        user: { type: Schema.Types.ObjectId, ref: "Profile", required: true }
+    },
+    { timestamps: true }
+)
+
+export default model('Posts', PostSchema)
