70338c9b75d69e4a04ac369c7d9b65eba8936d06
Hasan Sattar <hasanraza1798@gmail.com>
2022-01-17 10:50:36 +0000

    added error handdlers

---
+++ src/middlewares/errorHandlers.js
@@ -0,0 +1,18 @@
+export default errorHandlers = (err, req, res, next) => {
+    switch(err.name) {
+        case 'BadRequestError':
+        case 'ValidationError':
+        case 'CastError': 
+        case 'TypeError': 
+        case 'ObjectParameterError': 
+            res.status(400).send(err)
+            break
+        case 'UnauthorizedError': 
+            res.status(401).send(err)
+        case 'NotFoundError':
+            res.status(404).send(err)
+            break
+        default: 
+            res.status(500).send('Server Error')
+    }
+}
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import experienceRouter from "./Profiles/experienece.js";
 import postsRouter from "./Posts/posts.js";
 import profilesRouter from "./Profiles/experienece.js";
+import errorHandlers from "./middlewares/errorHandlers.js";
 
 const server = express();
 const PORT = process.env.PORT;
@@ -14,6 +15,8 @@ server.use(express.json());
 
 server.use("/posts", postsRouter);
 server.use("/profiles", profilesRouter, experienceRouter);
+
+server.use(errorHandlers)
 
 mongoose.connect(process.env.MONGO_CONNECTION);
 mongoose.connection.on("connected", () => {
