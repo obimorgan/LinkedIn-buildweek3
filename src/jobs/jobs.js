b9f91e06bc5194b8c8ede07d61712dd6b2b5109d
Hasan Sattar <hasanraza1798@gmail.com>
2022-01-18 10:28:06 +0000

    added error handlers and query for get route

--- src/jobs/jobs.js
+++ src/jobs/jobs.js
@@ -1,21 +1,33 @@
 import { Router } from 'express'
 import jobsModel from './schema.js'
+import createHttpError from 'http-errors'
+import q2m from 'query-to-mongo'
 
 const jobsRouter = Router()
 
 jobsRouter.route('/')
 .get(async (req, res, next) => {
     try {
-        const jobs = await jobsModel.find()
-        res.send(jobs)
+        const query = q2m(req.query)
+        console.log(query);
+        const { criteria, options, links } = query
+        const { sort, skip, limit } = options
+        console.log(links)
+        const totalJobs = await jobsModel.countDocuments(criteria)
+        const jobs = await jobsModel.find(criteria)
+        .sort(sort)
+        .skip(skip)
+        .limit(limit || 10)
+        const pages = Math.ceil(totalJobs / limit || 10)
+        // console.log(links('/jobs', totalJobs))
+        res.send({ totalJobs, pages, jobs })
     } catch (error) {
         next(error)
     }
 })
 .post(async (req, res, next) => {
     try {
-        const job = await new jobsModel(req.body)
-        await job.save()
+        const job = await new jobsModel(req.body).save()
         res.status(201).send(job)
     } catch (error) {
         next(error)
@@ -25,7 +37,9 @@ jobsRouter.route('/')
 jobsRouter.route('/:jobId')
 .get(async (req, res, next) => {
     try {
-        const job = await jobsModel.findById(req.params.jobId)
+        const { jobId } = req.params
+        const job = await jobsModel.findById(jobId)
+        if (!job) return next(createHttpError(404, `The Job with id ${jobId} does not exist.`))
         res.send(job)
     } catch (error) {
         next(error)
@@ -33,7 +47,9 @@ jobsRouter.route('/:jobId')
 })
 .put(async (req, res, next) => {
     try {
-        const job = await jobsModel.findByIdAndUpdate(req.params.jobId, req.body, { new: true, runValidators: true })
+        const { jobId } = req.params
+        const job = await jobsModel.findByIdAndUpdate(jobId, req.body, { new: true, runValidators: true })
+        if (!job) return next(createHttpError(404, `The Job with id ${jobId} does not exist.`))
         res.send(job)
     } catch (error) {
         next(error)
@@ -41,7 +57,9 @@ jobsRouter.route('/:jobId')
 })
 .delete(async (req, res, next) => {
     try {
-        const job = await jobsModel.findByIdAndDelete(req.params.jobId)
+        const { jobId } = req.params
+        const job = await jobsModel.findByIdAndDelete(jobId)
+        if (!job) return next(createHttpError(404, `The Job with id ${jobId} does not exist.`))
         res.sendStatus(204)
     } catch (error) {
         next(error)
